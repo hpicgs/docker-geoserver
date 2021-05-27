4dc29aab6518aaf28319d92dc5774afe61234296
Jan Klimke <jan.klimke@hpi.de>
2021-05-27 10:34:50 +0200

    updated plugin, cleaned up build script

--- build.sh
+++ build.sh
@@ -4,10 +4,8 @@ MAJOR=2
 MINOR=18
 BUGFIX=1
 
-
 # Build Geoserver
 echo "Building GeoServer ${MAJOR}.${MINOR}.${BUGFIX} "
-
 
 docker build --build-arg GS_VERSION=${MAJOR}.${MINOR}.${BUGFIX} -t hpicgs/ejpgeoserver:${MAJOR}.${MINOR}.${BUGFIX} .
 #docker-registry.hpi3d.de/ejp/ejpgeoserver-docker:${MAJOR}.${MINOR}.${BUGFIX} .
@@ -18,7 +16,3 @@ docker build --build-arg GS_VERSION=${MAJOR}.${MINOR}.${BUGFIX} -t hpicgs/ejpgeo
 #--build-arg WAR_URL=http://downloads.sourceforge.net/project/geoserver/GeoServer/<GS_VERSION>/geoserver-<GS_VERSION>-war.zip
 #--build-arg INITIAL_MEMORY=2G
 #--build-arg MAXIMUM_MEMORY=8G
-
-
-
-
